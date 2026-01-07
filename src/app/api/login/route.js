3868385be3860c1400a27788264e30f707b4a5b2
Manish Sonawane <manishsonawane3010@gmail.com>
2026-01-07 04:07:17 +0000

    ok i have registered 2 users as examinars they have approval status as t

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -26,8 +26,8 @@ export async function POST(req) {
              return NextResponse.json({ message: `You are not registered as a(n) ${role}.` }, { status: 403 });
         }
 
-        // Check if user is approved (especially for 'examiner')
-        if (user.role === 'examiner') {
+        // Check if user is approved (especially for 'examinar')
+        if (user.role === 'examinar') {
             if (user.approvalStatus === 'Pending') {
                 return NextResponse.json({ message: 'Your account is pending approval by an administrator.' }, { status: 403 });
             }
